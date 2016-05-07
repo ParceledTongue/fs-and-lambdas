58566a93cc38e703709ceafe263f192ec5365b2a
Zachary Palumbo <ztp3@case.edu>
2016-05-07 05:05:54 -0400

    Created function to convert board from three-character alphabetic representation to list representation
    Signed-off-by: Zachary Palumbo <ztp3@case.edu>

--- fal.rkt
+++ fal.rkt
@@ -12,8 +12,6 @@
 ;  O |   |
 
 
-
-
 ; ; ; ; ; ;
 ; DISPLAY ;
 ; ; ; ; ; ;
@@ -24,7 +22,7 @@
         (newline)
         ; if there is still board info to print:
         (begin
-          (display " ") (display (string-rep (car b)))
+          (display " ") (display (symbol-to-string (car b)))
           (if (or (eq? (length b) 7) (eq? (length b) 4))
               ; if this is the last symbol of the first or second row, display
               ; the horizontal break in the board
@@ -36,13 +34,14 @@
                   (display-board (cdr b))
                   (begin (display " |") (display-board (cdr b)))))))))
 
-(define symbol-string-rep
+(define symbol-to-string
   (lambda (s)
     (cond
       ((eq? s 'o) "O")
       ((eq? s 'x) "X")
       ((eq? s 'n) " ")
       (else (error s "Invalid board symbol")))))
+
 
 ; ; ; ; ; ; ; ;
 ; BOARD CODES ;
@@ -52,12 +51,27 @@
   (lambda (b)
     (if (null? b)
         ""
-        (string-append (list-ref alphabet (+ (* 9 (symbol-number-rep (car b)))
-                                             (* 3 (symbol-number-rep (cadr b)))
-                                             (symbol-number-rep (caddr b))))
+        (string-append (list-ref alphabet (+ (* 9 (symbol-to-number (car b)))
+                                             (* 3 (symbol-to-number (cadr b)))
+                                             (symbol-to-number (caddr b))))
                        (board-to-code (cdddr b))))))
 
-(define symbol-number-rep
+(define code-to-board
+  (lambda (c)
+    (if (string=? c "")
+        '()
+        (append (letter-to-board-row (substring c 0 1))
+                (code-to-board (substring c 1))))))
+
+(define letter-to-board-row
+  (lambda (l)
+    ((lambda (n)
+       (list (number-to-symbol (remainder (truncate (/ n 9)) 3))
+             (number-to-symbol (remainder (truncate (/ n 3)) 3))
+             (number-to-symbol (remainder n 3))))
+     (index-of l alphabet))))
+
+(define symbol-to-number
   (lambda (s)
     (cond
       ((eq? s 'o) 0)
@@ -65,7 +79,31 @@
       ((eq? s 'n) 2)
       (else (error s "Invalid board symbol")))))
 
+(define number-to-symbol
+  (lambda (n)
+    (cond
+      ((eq? n 0) 'o)
+      ((eq? n 1) 'x)
+      ((eq? n 2) 'n)
+      (else (error n "Invalid number")))))
+
 (define alphabet '("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o"
                        "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "0"))
 ; (if only the Latin alphabet had 27 letters instead of only 26,
 ; we wouldn't need that silly 0)
+
+
+; ; ; ; ;
+; MISC. ;
+; ; ; ; ;
+
+(define index-of
+  (lambda (a l)
+    (call/cc
+     (lambda (return)
+       (letrec ([index-of* (lambda (a l)
+                             (cond
+                               ((null? l) (return -1))
+                               ((string=? a (car l)) 0)
+                               (else (+ 1 (index-of* a (cdr l))))))])
+         (index-of* a l))))))
